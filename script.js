7479c9db2afaadd1202f6826e217159285195020
nicollycruzbarbosa <nicollybarbosaeduc@gmail.com>
2025-03-31 12:09:59 -0300

    Arquivo ART

---
+++ script.js
@@ -0,0 +1,6 @@
+// ======= Alternar modo claro / modo escuro
+// Função para alterar entre os temas
+function toggleMode(){
+    const html = document.documentElement;
+    html.classList.toggle('light');
+}
